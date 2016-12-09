660ccdfe9582f128c80e5eeb5a59f2eb228f5827
Adam and Nick <adam+nc@thoughtbot.com>
2016-12-09 10:20:45 -0500

    Add Xcode project

---
+++ Sources/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FinchUI
+//
+//  Created by Adam Sharp on 12/8/16.
+//  Copyright © 2016 thoughtbot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
