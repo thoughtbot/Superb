f24debdd1adc8bdb1ffbd6f24d22b84625a4ce90
Adam and Nick <adam+nc@thoughtbot.com>
2016-12-09 19:55:25 +0000

    Complete GitHub Basic Auth Provider.

--- Sources/Providers/GitHubBasicAuthProvider.swift
+++ Sources/Providers/GitHubBasicAuthProvider.swift
@@ -1,5 +1,7 @@
 import UIKit
 
+let createPersonalAccessTokenURL = URL(string: "https://api.github.com/authorizations")!
+
 final class GitHubBasicAuthProvider : FinchProvider {
   static let identifier = "com.thoughtbot.finch.github.basic"
 
@@ -25,8 +27,7 @@ final class GitHubBasicAuthProvider : FinchProvider {
     }
 
     let submit = UIAlertAction(title: "OK", style: .default) { _ in
-      print(self.login)
-      print(self.password)
+      self.createAccessToken(login: self.login, password: self.password, completionHandler: completionHandler)
     }
     alert.addAction(submit)
 
@@ -43,4 +44,61 @@ final class GitHubBasicAuthProvider : FinchProvider {
   @objc func passwordChanged(_ sender: UITextField) {
     password = sender.text ?? ""
   }
+
+  private func createAccessToken(login: String, password: String, completionHandler: @escaping (Result<String>) -> Void) {
+
+    let requestBody = try? JSONSerialization.data(withJSONObject: [
+      "note": makePersonalAccessTokenNote()
+    ])
+
+    var request = URLRequest(url: createPersonalAccessTokenURL)
+    request.httpBody = requestBody
+    request.httpMethod = "POST"
+    request.setValue(createAuthorizationHeader(login: login, password: password), forHTTPHeaderField: "Authorization")
+
+    let task = URLSession.shared.dataTask(with: request) { data, response, error in
+      var result: Result<String>!
+
+      defer {
+        DispatchQueue.main.async {
+          completionHandler(result)
+        }
+      }
+
+      guard error == nil else {
+        result = .failure(error!)
+        return
+      }
+
+      guard let data = data,
+        let object = try? JSONSerialization.jsonObject(with: data),
+        let response = object as? [String: Any],
+        let token = response["token"] as? String
+        else {
+          result = .failure(FinchError.authorizationResponseInvalid)
+          return
+        }
+
+      result = .success(token)
+    }
+
+    task.resume()
+  }
+
+  private func makePersonalAccessTokenNote() -> String {
+    let date = iso8601Formatter.string(from: Date())
+
+    return "Finch GitHub Basic Auth Sign In \(date)"
+  }
+
+  private func createAuthorizationHeader(login: String, password: String) -> String {
+    let rawValue = "\(login):\(password)".data(using: .utf8)!
+    return "Basic \(rawValue.base64EncodedString())"
+  }
+
+  private let iso8601Formatter: ISO8601DateFormatter = {
+    let formatter = ISO8601DateFormatter()
+    formatter.timeZone = .autoupdatingCurrent
+    return formatter
+  }()
 }
