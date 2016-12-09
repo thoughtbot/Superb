0787dbbced6ad772cf63aef578830157727b0ffa
Adam and Nick <adam+nc@thoughtbot.com>
2016-12-09 10:20:50 -0500

    Create access token after OAuth authorization callback

--- Sources/Providers/GitHubOAuthProvider.swift
+++ Sources/Providers/GitHubOAuthProvider.swift
@@ -2,12 +2,19 @@ import SafariServices
 import UIKit
 
 let authorizeURL = URL(string: "https://github.com/login/oauth/authorize?client_id=3127cd33caef9514cbc5&redirect_uri=finchui%3A%2F%2Foauth%2Fgithub%2Fcode")!
+let createAccessTokenURL = URL(string: "https://github.com/login/oauth/access_token")!
 
 final class GitHubOAuthProvider: FinchProvider {
   static let identifier = "com.thoughtbot.finch.github.oauth"
 
-  func authorize(over viewController: UIViewController) {
+  private var completionHandler: ((String?) -> Void)?
+  private var safariViewController: SFSafariViewController?
+
+  func authorize(over viewController: UIViewController, completionHandler: @escaping (String?) -> Void) {
+    self.completionHandler = completionHandler
+
     let safariViewController = SFSafariViewController(url: authorizeURL)
+    self.safariViewController = safariViewController
     viewController.present(safariViewController, animated: true)
   }
 
@@ -17,7 +24,59 @@ final class GitHubOAuthProvider: FinchProvider {
       let codeParam = query.first(where: { $0.name == "code" }),
       let code = codeParam.value
       else { return false }
-    print("received code", code)
+
+    var params: [URLQueryItem] = []
+    params.append(URLQueryItem(name: "code", value: code))
+    params.append(URLQueryItem(name: "client_secret", value: "b9d69d245ca6f952b50c77a543dca4f4c612ff73"))
+    params.append(URLQueryItem(name: "client_id", value: "3127cd33caef9514cbc5"))
+
+    var requestComponents = URLComponents()
+    requestComponents.queryItems = params
+
+    let requestQuery = requestComponents.query!
+    let requestBody = requestQuery.data(using: .utf8)!
+
+    var request = URLRequest(url: createAccessTokenURL)
+    request.httpBody = requestBody
+    request.httpMethod = "POST"
+
+    let task = URLSession.shared.dataTask(with: request) { [weak self] data, response, error in
+      guard let `self` = self, let complete = self.completionHandler else { return }
+
+      defer { self.completionHandler = nil }
+
+      var tokenResult: String?
+
+      defer {
+        DispatchQueue.main.async { [safariViewController = self.safariViewController] in
+          if let safariViewController = safariViewController {
+            safariViewController.dismiss(animated: true) {
+              complete(tokenResult)
+            }
+          } else {
+            complete(tokenResult)
+          }
+        }
+      }
+
+      guard error == nil else {
+        return
+      }
+
+      let response = data.flatMap { String(data: $0, encoding: .utf8) }
+
+      var components = URLComponents()
+      components.query = response
+
+      guard let token = components.queryItems?.first(where: { $0.name == "access_token" })?.value else {
+        return
+      }
+
+      tokenResult = token
+    }
+
+    task.resume()
+
     return true
   }
 }
--- Sources/Providers/GitHubOAuthViewController.swift
+++ Sources/Providers/GitHubOAuthViewController.swift
@@ -2,6 +2,28 @@ import UIKit
 
 final class GitHubOAuthViewController: UIViewController {
   @IBAction func authorize(_ sender: Any?) {
-    AppDelegate.gitHubProvider.authorize(over: self)
+    AppDelegate.gitHubProvider.authorize(over: self) { [weak self] token in
+      guard let `self` = self else { return }
+
+      let alert: UIAlertController
+
+      if let token = token {
+        print("authorized with token ", token)
+
+        alert = UIAlertController(title: "Authorized!", message: "Token: \(token)", preferredStyle: .alert)
+
+        let dismiss = UIAlertAction(title: "OK", style: .default) { _ in
+          self.performSegue(withIdentifier: "unwindToProviders", sender: self)
+        }
+        alert.addAction(dismiss)
+      } else {
+        alert = UIAlertController(title: "Authorization Failed", message: "😢", preferredStyle: .alert)
+
+        let confirm = UIAlertAction(title: "OK", style: .cancel)
+        alert.addAction(confirm)
+      }
+
+      self.present(alert, animated: true)
+    }
   }
 }
