a6820f4e6d6e9211cd6be013ddf7494f848c1116
Adam Sharp <adam@sharplet.me>
2017-02-01 13:27:19 -0500

    Separate provider registration from protocol definitions

--- Sources/Finch/AuthenticationProvider.swift
+++ Sources/Finch/AuthenticationProvider.swift
@@ -1,37 +1,6 @@
 import Result
 import UIKit
 
-public enum Finch {
-  private static var providers: [String: Any] = [:]
-
-  public static func register<Provider: AuthenticationProvider>(_ makeProvider: @autoclosure () -> Provider) -> Provider {
-    return register { makeProvider() }
-  }
-
-  public static func register<Provider: AuthenticationProvider>(makeProvider: () -> Provider) -> Provider {
-    let id = Provider.identifier
-
-    if let instance = providers[id] {
-      return instance as! Provider
-    } else {
-      let instance = makeProvider()
-      providers[id] = instance
-      return instance
-    }
-  }
-
-  public static func handleAuthenticationRedirect(_ url: URL, options: [UIApplicationOpenURLOptionsKey: Any]) -> Bool {
-    for (_, provider) in providers {
-      let handler = provider as! _CallbackHandler
-      if handler.handleCallback(url, options: options) {
-        return true
-      }
-    }
-
-    return false
-  }
-}
-
 public protocol _CallbackHandler {
   func handleCallback(_ url: URL, options: [UIApplicationOpenURLOptionsKey: Any]) -> Bool
 }
