abd67f1d9302dcf53e09828a3aa2c7fc900147ec
Adam Sharp <adam@sharplet.me>
2017-05-15 16:17:21 -0400

    Use grouped table view to organise demo providers
    This will provide some more structure for when we add new providers in
    the future, and looks a lot classier.

--- Sources/SuperbDemo/ProvidersViewController.swift
+++ Sources/SuperbDemo/ProvidersViewController.swift
@@ -1,6 +1,11 @@
 import UIKit
 
-final class ProvidersViewController: UIViewController {
+final class ProvidersViewController: UITableViewController {
   @IBAction func unwindToProviders(_ segue: UIStoryboardSegue) {
+  }
+
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    guard let identifier = tableView.cellForRow(at: indexPath)?.textLabel?.text else { return }
+    performSegue(withIdentifier: identifier, sender: nil)
   }
 }
